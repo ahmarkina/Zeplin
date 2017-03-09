1a8995dc918d0fa1fc39c3123dd610ac705967e2
Anastasiia Markina <ahmarkina@gmail.com>
2017-03-10 02:11:21 +0400

    first commit, in progress

--- build/js/script.js
+++ build/js/script.js
@@ -2,32 +2,12 @@
  * Created by anastasia.markina on 2016-12-10.
  */
 $(document).ready(function(){
-    /*$('.WrapSliderLeftSide').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });*/
 
-    $('.WrapSliderLeft').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        fade: false,
-        speed: 500,
-        infinite: true,
-        asNavFor: '.WrapSliderRight'
-    });
-
-    $('.WrapSliderRight').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.WrapSliderLeft',
-        dots: false,
-        fade: true,
-        infinity: true,
-        focusOnSelect: true
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 7,
+        arrows: true,
+        dots: false
     });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,12 @@
  * Created by anastasia.markina on 2016-12-10.
  */
 $(document).ready(function(){
-    $('.WrapSlider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 7,
+        arrows: true,
+        dots: false
     });
 });
